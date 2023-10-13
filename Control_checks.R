1b9b0d080395ff4a9d7a69694e5f8b2e44e3b538
Pablo Martínez Sosa <martoza@gmail.com>
2023-10-13 11:24:51 +0200

    Update Control_checks.R

--- Control_checks.R
+++ Control_checks.R
@@ -1,3 +1,6 @@
+# maybe useful down the line to manipulate whole groups of GDGTs
+GDGTs<-list(isoGDGTs=list("GDGT0","GDGT1","GDGT2","GDGT3","Crena","Crenp"),brGDGTs=list("IIIa5","IIIa6","IIIb5","IIIb6","IIIc5","IIIc6","IIa5","IIa6","IIb5","IIb6","IIc5","IIc6","Ia","Ib","Ic"))
+
 correctGs<-function(eq){
   sub("fa.","",unlist(strsplit(eq," "))[grep("fa",unlist(strsplit(eq," ")))])
 }
